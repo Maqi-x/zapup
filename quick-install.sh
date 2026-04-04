11109cdab5ecc075d821ebbde8b473a40a6161a4
Maqi-x <186316794+maqi-x@users.noreply.github.com>
2026-04-04 18:14:16 +0200

    init(quick-install): add quick-install script

---
+++ quick-install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -e
+
+TMP_DIR=$(mktemp -d)
+
+git clone https://github.com/Maqi-x/zapup.git "$TMP_DIR/zapup"
+
+cd "$TMP_DIR/zapup"
+
+make
+sudo make install
+
+cd /
+rm -rf "$TMP_DIR"
