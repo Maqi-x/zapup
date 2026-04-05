20d27fb26cdccc6cac25c52c0cd25141cb0ce3df
Maqi-x <186316794+maqi-x@users.noreply.github.com>
2026-04-05 14:04:30 +0200

    feat(util/fs): add more fs utility functions
    - Added functions to create directories and files if they do not exist
    - Added functions to remove files and directories
    - Added enum for file types
    - Added function to check if a file exists and retrieve its type

--- include/util/fs.h
+++ include/util/fs.h
@@ -5,6 +5,23 @@
 bool z_mkdir(ZPathView path);
 bool z_mkdir_all(ZPathView path);
 bool z_mkfile(ZPathView path);
+
+bool Z_mkdir_if_not_exists();
+bool z_mkfile_if_not_exists();
+
 bool z_touch(ZPathView path);
+bool z_rm(ZPathView path);
+bool z_rm_recursive(ZPathView path);
+
+typedef enum ZFileType {
+    Z_FILE_REGULAR,
+    Z_FILE_DIR,
+    Z_FILE_SYMLINK,
+    Z_FILE_OTHER,
+    Z_FILE_NOT_FOUND,
+} ZFileType;
+
+bool z_file_exists(ZPathView path);
+ZFileType z_get_file_type(ZPathView path);
 
 bool z_set_executable(ZPathView path, bool enabled);
