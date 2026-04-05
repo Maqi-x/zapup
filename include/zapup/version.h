dac21e18b7769e051b0ab1d478ece5b16b825cc1
Maqi-x <186316794+maqi-x@users.noreply.github.com>
2026-04-05 21:21:31 +0200

    feat(zapup/version): add z_zap_ver_is_null function

--- include/zapup/version.h
+++ include/zapup/version.h
@@ -9,4 +9,8 @@ typedef struct ZResolvableZapVersion {
 
 #define Z_ZAP_VERSION_NULL ((ZResolvableZapVersion) { .branch = Z_SV_NULL, .commit = Z_SV_NULL })
 
+static inline bool z_zap_ver_is_null(ZResolvableZapVersion v) {
+    return z_sv_is_null(v.branch) && z_sv_is_null(v.commit);
+}
+
 ZResolvableZapVersion z_parse_zap_version(ZStringView s);
