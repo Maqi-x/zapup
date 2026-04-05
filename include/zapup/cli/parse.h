f59b8cc805ffadd4aa182e4a53e0e27114c170db
Maqi-x <186316794+maqi-x@users.noreply.github.com>
2026-04-05 19:37:05 +0200

    feat(zapup/cli): update error handling defs

--- include/zapup/cli/parse.h
+++ include/zapup/cli/parse.h
@@ -3,6 +3,7 @@
 #include <zapup/cli/args.h>
 
 typedef enum ZCliParseErrorCode {
+    Z_CLI_PARSE_OK,
     Z_CLI_PARSE_UNKNOWN_FLAG,
     Z_CLI_PARSE_UNKNOWN_COMMAND,
     // TODO
@@ -11,8 +12,10 @@ typedef enum ZCliParseErrorCode {
 typedef struct ZCliParseError {
     ZCliParseErrorCode code;
     union {
-        // TODO
-    };
+        ZStringView str;
+    } ctx;
 } ZCliParseError;
+
+#define Z_CLI_PARSE_MK_OK ((ZCliParseError) { .code = Z_CLI_PARSE_OK })
 
 ZCliParseError z_cli_parse_args(int argc, const char* const* argv, ZCliArgs* out);
