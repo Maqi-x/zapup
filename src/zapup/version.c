839b5db2db0dbeae405f9076b77fde4921625c51
Maqi-x <186316794+maqi-x@users.noreply.github.com>
2026-04-06 15:08:29 +0200

    feat(zapup/version): update version parsing logic to support build type

--- src/zapup/version.c
+++ src/zapup/version.c
@@ -5,23 +5,27 @@ ZResolvableZapVersion z_parse_zap_version(ZStringView s) {
         return Z_ZAP_VERSION_NULL;
     }
 
-    usize sepIndex = s.len;
-    for (usize i = 0; i < s.len; ++i) {
-        if (s.data[i] == '@') {
-            sepIndex = i;
+    ZResolvableZapVersion v = {
+        .branch = Z_SV_NULL,
+        .commit = s,
+        .build = Z_BUILD_RELEASE,
+    };
+
+    if (z_sv_ends_with(s, Z_SV(":debug"))) {
+        v.build = Z_BUILD_DEBUG;
+        v.commit.len -= 6;
+    } else if (z_sv_ends_with(s, Z_SV(":release"))) {
+        v.build = Z_BUILD_RELEASE;
+        v.commit.len -= 8;
+    }
+
+    for (usize i = 0; i < v.commit.len; ++i) {
+        if (v.commit.data[i] == '@') {
+            v.branch = z_sv_slice(v.commit, 0, i);
+            v.commit = z_sv_slice(v.commit, i + 1, v.commit.len);
             break;
         }
     }
-    
-    if (sepIndex == s.len) {
-        return (ZResolvableZapVersion) {
-            .branch = Z_SV_NULL,
-            .commit = s,
-        };
-    }
-    
-    return (ZResolvableZapVersion) {
-        .branch = z_sv_slice(s, 0, sepIndex),
-        .commit = z_sv_slice(s, sepIndex + 1, s.len),
-    };
+
+    return v;
 }
