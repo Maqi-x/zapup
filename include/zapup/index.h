b3802ef64ca8a3243ff6d1d5650210ab958ea1f3
Maqi-x <186316794+maqi-x@users.noreply.github.com>
2026-04-06 07:01:48 +0200

    feat(zapup/index): add version index header file

---
+++ include/zapup/index.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <util/pathview.h>
+#include <util/strbuf.h>
+
+#include <zapup/version.h>
+#include <defs/int-types.h>
+
+typedef struct ZVersionIndexEntry {
+    ZResolvableZapVersion version;
+    ZPathView path;
+} ZVersionIndexEntry;
+
+typedef struct ZVersionIndex {
+    ZVersionIndexEntry* entries;
+    usize len, cap;
+    void* _ctx;
+} ZVersionIndex;
+
+void z_version_index_init(ZVersionIndex* idx);
+void z_version_index_free(ZVersionIndex* idx);
+
+void z_version_index_add(ZVersionIndex* idx, ZResolvableZapVersion version, ZPathView path);
+
+void z_version_index_from_json(ZVersionIndex* idx, ZStringView json);
+bool z_version_index_to_json(ZVersionIndex* idx, ZStringBuf* out);
