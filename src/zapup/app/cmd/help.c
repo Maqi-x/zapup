69fcb357f582784c61b89191e33c142eea8e79e5
Maqi-x <186316794+maqi-x@users.noreply.github.com>
2026-04-07 14:56:45 +0200

    feat(zapup/cmd/help): add help implementation

--- src/zapup/app/cmd/help.c
+++ src/zapup/app/cmd/help.c
@@ -1,9 +1,13 @@
 #include <zapup/app/app.h>
 
+#include <zapup/cli/help/settings.h>
+#include <zapup/cli/help/print.h>
+
 #include <zapup/output.h>
 
 int zapup_exec_help(ZapupApp* app) {
     (void) app;
-    z_show_warn("help: not implemented yet");
+    ZHelpInfo info = zapup_get_help();
+    z_cli_print_help(&info, stdout);
     return 0;
 }
