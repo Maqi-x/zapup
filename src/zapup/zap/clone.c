2ab6c5ecc8e6ab933204ab27a469b127d66eb0cb
Maqi-x <186316794+maqi-x@users.noreply.github.com>
2026-04-07 18:45:30 +0200

    feat(zapup/zap/clone): allow passing NULL as out_repo

--- src/zapup/zap/clone.c
+++ src/zapup/zap/clone.c
@@ -7,10 +7,13 @@
 
 int z_clone_zap_repo_with_version(ZResolvableZapVersion ver, ZPathView path, git_repository** out_repo) {
     int error = 0;
+    if (out_repo) *out_repo = NULL;
+
     char* path_cstr = z_sv_to_cstr_alloc(path);
     char* branch_cstr = NULL;
     char* revspec_cstr = NULL;
     git_object* commit_obj = NULL;
+    git_repository* repo = NULL;
 
     if (!path_cstr) return -1;
 
@@ -26,30 +29,37 @@ int z_clone_zap_repo_with_version(ZResolvableZapVersion ver, ZPathView path, git
         clone_opts.checkout_branch = branch_cstr;
     }
 
-    error = git_clone(out_repo, Z_ZAP_REPO_URL, path_cstr, &clone_opts);
+    error = git_clone(&repo, Z_ZAP_REPO_URL, path_cstr, &clone_opts);
     if (error != 0) goto cleanup;
 
-    if (z_sv_is_null(ver.revspec)) goto cleanup;
+    if (!z_sv_is_null(ver.revspec)) {
+        revspec_cstr = z_sv_to_cstr_alloc(ver.revspec);
+        if (!revspec_cstr) {
+            error = -1;
+            goto cleanup;
+        }
 
-    revspec_cstr = z_sv_to_cstr_alloc(ver.revspec);
-    if (!revspec_cstr) {
-        error = -1;
-        goto cleanup;
+        error = git_revparse_single(&commit_obj, repo, revspec_cstr);
+        if (error != 0) goto cleanup;
+
+        git_checkout_options checkout_opts;
+        git_checkout_options_init(&checkout_opts, GIT_CHECKOUT_OPTIONS_VERSION);
+        checkout_opts.checkout_strategy = GIT_CHECKOUT_FORCE;
+
+        error = git_checkout_tree(repo, commit_obj, &checkout_opts);
+        if (error != 0) goto cleanup;
+
+        error = git_repository_set_head_detached(repo, git_object_id(commit_obj));
+        if (error != 0) goto cleanup;
     }
 
-    error = git_revparse_single(&commit_obj, *out_repo, revspec_cstr);
-    if (error != 0) goto cleanup;
-
-    git_checkout_options checkout_opts;
-    git_checkout_options_init(&checkout_opts, GIT_CHECKOUT_OPTIONS_VERSION);
-    checkout_opts.checkout_strategy = GIT_CHECKOUT_FORCE;
-
-    error = git_checkout_tree(*out_repo, commit_obj, &checkout_opts);
-    if (error != 0) goto cleanup;
-
-    error = git_repository_set_head_detached(*out_repo, git_object_id(commit_obj));
+    if (out_repo) {
+        *out_repo = repo;
+        repo = NULL;
+    }
 
 cleanup:
+    if (repo) git_repository_free(repo);
     git_object_free(commit_obj);
     free(revspec_cstr);
     free(branch_cstr);
