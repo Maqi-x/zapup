35aee58ea1bb7f04db33f6addfcfc0ce97b73b07
Maqi-x <186316794+maqi-x@users.noreply.github.com>
2026-04-08 14:45:41 +0200

    fix(zapup/zap/sync): fix sync when HEAD is deteached

--- src/zapup/zap/sync.c
+++ src/zapup/zap/sync.c
@@ -111,6 +111,10 @@ int z_sync_zap_repo_with_version(ZResolvableZapVersion ver, ZPathView path, git_
     git_reference* remote_ref = NULL;
     git_reference* moved_ref = NULL;
     char* remote_ref_name = NULL;
+    char* branch_short_cstr = NULL;
+    char* local_branch_refname = NULL;
+    git_reference* local_branch_ref = NULL;
+    bool detached_mode = false;
 
     if (out_updated) *out_updated = false;
 
@@ -138,8 +142,29 @@ int z_sync_zap_repo_with_version(ZResolvableZapVersion ver, ZPathView path, git_
         const char* resolved_name = git_reference_name(resolved_head);
         remote_ref_name = build_remote_tracking_refname(resolved_name);
         if (!remote_ref_name) {
-            err = 0;
-            goto cleanup;
+            const char* branch_short = NULL;
+            if (!z_sv_is_null(ver.branch)) {
+                branch_short_cstr = z_sv_to_cstr_alloc(ver.branch);
+                if (!branch_short_cstr) { err = -1; goto cleanup; }
+                branch_short = branch_short_cstr;
+            } else {
+                branch_short = "main";
+            }
+            size_t rlen = strlen("refs/remotes/origin/") + strlen(branch_short) + 1;
+            remote_ref_name = malloc(rlen);
+            if (!remote_ref_name) { err = -1; goto cleanup; }
+            memcpy(remote_ref_name, "refs/remotes/origin/", strlen("refs/remotes/origin/"));
+            memcpy(remote_ref_name + strlen("refs/remotes/origin/"), branch_short, strlen(branch_short));
+            remote_ref_name[rlen - 1] = '\0';
+
+            size_t llen = strlen("refs/heads/") + strlen(branch_short) + 1;
+            local_branch_refname = malloc(llen);
+            if (!local_branch_refname) { err = -1; goto cleanup; }
+            memcpy(local_branch_refname, "refs/heads/", strlen("refs/heads/"));
+            memcpy(local_branch_refname + strlen("refs/heads/"), branch_short, strlen(branch_short));
+            local_branch_refname[llen - 1] = '\0';
+
+            detached_mode = true;
         }
     }
 
@@ -150,7 +175,7 @@ int z_sync_zap_repo_with_version(ZResolvableZapVersion ver, ZPathView path, git_
         goto cleanup;
     }
 
-    {
+    if (!detached_mode) {
         int cmp = compare_ref_oids(resolved_head, remote_ref);
         if (cmp == -1) {
             err = 0;
@@ -160,20 +185,65 @@ int z_sync_zap_repo_with_version(ZResolvableZapVersion ver, ZPathView path, git_
             err = 0;
             goto cleanup;
         }
-    }
 
-    {
+        {
+            const git_oid* remote_oid = git_reference_target(remote_ref);
+            err = checkout_and_move_ref(repo, remote_oid, resolved_head, &moved_ref);
+            if (err != 0) goto cleanup;
+            if (out_updated) *out_updated = true;
+        }
+    } else {
         const git_oid* remote_oid = git_reference_target(remote_ref);
-        err = checkout_and_move_ref(repo, remote_oid, resolved_head, &moved_ref);
+        const git_oid* head_oid = git_reference_target(resolved_head);
+        if (!remote_oid || !head_oid) { err = 0; goto cleanup; }
+        if (git_oid_cmp(head_oid, remote_oid) == 0) {
+            if (out_updated) *out_updated = false;
+            err = 0;
+            goto cleanup;
+        }
+
+        git_object* commit_obj = NULL;
+        err = git_object_lookup(&commit_obj, repo, remote_oid, GIT_OBJECT_COMMIT);
         if (err != 0) goto cleanup;
+
+        git_checkout_options checkout_opts;
+        git_checkout_options_init(&checkout_opts, GIT_CHECKOUT_OPTIONS_VERSION);
+        checkout_opts.checkout_strategy = GIT_CHECKOUT_FORCE;
+
+        err = git_checkout_tree(repo, commit_obj, &checkout_opts);
+        if (err != 0) {
+            git_object_free(commit_obj);
+            goto cleanup;
+        }
+
+        err = git_reference_lookup(&local_branch_ref, repo, local_branch_refname);
+        if (err != 0) {
+            giterr_clear();
+            local_branch_ref = NULL;
+        }
+
+        if (local_branch_ref) {
+            err = git_reference_set_target(&moved_ref, local_branch_ref, remote_oid, "zapup: fast-forward");
+        } else {
+            err = git_reference_create(&moved_ref, repo, local_branch_refname, remote_oid, 1, "zapup: create branch");
+        }
+        git_object_free(commit_obj);
+        if (err != 0) goto cleanup;
+
+        err = git_repository_set_head(repo, local_branch_refname);
+        if (err != 0) goto cleanup;
+
         if (out_updated) *out_updated = true;
     }
 
 cleanup:
     if (moved_ref) git_reference_free(moved_ref);
     if (remote_ref) git_reference_free(remote_ref);
+    if (local_branch_ref) git_reference_free(local_branch_ref);
     if (resolved_head) git_reference_free(resolved_head);
     free(remote_ref_name);
+    free(local_branch_refname);
+    free(branch_short_cstr);
 
     if (err != 0) {
         if (repo && out_repo == NULL) {
