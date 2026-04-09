a797f92b632434ec361b6bd02550b5ba601ba17d
Maqi-x <186316794+maqi-x@users.noreply.github.com>
2026-04-09 14:57:45 +0200

    fix(tests/version): fix version test

--- tests/test-version.c
+++ tests/test-version.c
@@ -9,42 +9,42 @@ void tearDown(void) {}
 void test_parse_commit_only(void) {
     ZapVersion v = z_parse_zap_version(Z_SV("29be27b"));
     TEST_ASSERT_TRUE(z_sv_is_null(v.branch));
-    TEST_ASSERT_EQUAL_SV(Z_SV("29be27b"), v.commit);
+    TEST_ASSERT_EQUAL_SV(Z_SV("29be27b"), v.revspec);
     TEST_ASSERT_EQUAL_INT(Z_BUILD_RELEASE, v.build);
 }
 
 void test_parse_branch_and_commit(void) {
     ZapVersion v = z_parse_zap_version(Z_SV("main@HEAD"));
     TEST_ASSERT_EQUAL_SV(Z_SV("main"), v.branch);
-    TEST_ASSERT_EQUAL_SV(Z_SV("HEAD"), v.commit);
+    TEST_ASSERT_EQUAL_SV(Z_SV("HEAD"), v.revspec);
     TEST_ASSERT_EQUAL_INT(Z_BUILD_RELEASE, v.build);
 }
 
 void test_parse_commit_release(void) {
     ZapVersion v = z_parse_zap_version(Z_SV("7891981:release"));
     TEST_ASSERT_TRUE(z_sv_is_null(v.branch));
-    TEST_ASSERT_EQUAL_SV(Z_SV("7891981"), v.commit);
+    TEST_ASSERT_EQUAL_SV(Z_SV("7891981"), v.revspec);
     TEST_ASSERT_EQUAL_INT(Z_BUILD_RELEASE, v.build);
 }
 
 void test_parse_commit_debug(void) {
     ZapVersion v = z_parse_zap_version(Z_SV("c0200e9:debug"));
     TEST_ASSERT_TRUE(z_sv_is_null(v.branch));
-    TEST_ASSERT_EQUAL_SV(Z_SV("c0200e9"), v.commit);
+    TEST_ASSERT_EQUAL_SV(Z_SV("c0200e9"), v.revspec);
     TEST_ASSERT_EQUAL_INT(Z_BUILD_DEBUG, v.build);
 }
 
 void test_parse_branch_commit_debug(void) {
     ZapVersion v = z_parse_zap_version(Z_SV("main@HEAD:debug"));
     TEST_ASSERT_EQUAL_SV(Z_SV("main"), v.branch);
-    TEST_ASSERT_EQUAL_SV(Z_SV("HEAD"), v.commit);
+    TEST_ASSERT_EQUAL_SV(Z_SV("HEAD"), v.revspec);
     TEST_ASSERT_EQUAL_INT(Z_BUILD_DEBUG, v.build);
 }
 
 void test_parse_branch_commit_release(void) {
     ZapVersion v = z_parse_zap_version(Z_SV("feat@ea1f166:release"));
     TEST_ASSERT_EQUAL_SV(Z_SV("feat"), v.branch);
-    TEST_ASSERT_EQUAL_SV(Z_SV("ea1f166"), v.commit);
+    TEST_ASSERT_EQUAL_SV(Z_SV("ea1f166"), v.revspec);
     TEST_ASSERT_EQUAL_INT(Z_BUILD_RELEASE, v.build);
 }
 
@@ -69,7 +69,7 @@ void test_parse_invalid_at_only(void) {
 }
 
 void test_format_commit_only(void) {
-    ZapVersion v = { .branch = Z_SV_NULL, .commit = Z_SV("29be27b"), .build = Z_BUILD_RELEASE };
+    ZapVersion v = { .branch = Z_SV_NULL, .revspec = Z_SV("29be27b"), .build = Z_BUILD_RELEASE };
     ZStringBuf sb;
     z_strbuf_init(&sb);
     z_format_zap_version(v, &sb);
@@ -78,7 +78,7 @@ void test_format_commit_only(void) {
 }
 
 void test_format_branch_and_commit(void) {
-    ZapVersion v = { .branch = Z_SV("main"), .commit = Z_SV("HEAD"), .build = Z_BUILD_RELEASE };
+    ZapVersion v = { .branch = Z_SV("main"), .revspec = Z_SV("HEAD"), .build = Z_BUILD_RELEASE };
     ZStringBuf sb;
     z_strbuf_init(&sb);
     z_format_zap_version(v, &sb);
@@ -87,7 +87,7 @@ void test_format_branch_and_commit(void) {
 }
 
 void test_format_commit_debug(void) {
-    ZapVersion v = { .branch = Z_SV_NULL, .commit = Z_SV("c0200e9"), .build = Z_BUILD_DEBUG };
+    ZapVersion v = { .branch = Z_SV_NULL, .revspec = Z_SV("c0200e9"), .build = Z_BUILD_DEBUG };
     ZStringBuf sb;
     z_strbuf_init(&sb);
     z_format_zap_version(v, &sb);
@@ -96,7 +96,7 @@ void test_format_commit_debug(void) {
 }
 
 void test_format_branch_commit_debug(void) {
-    ZapVersion v = { .branch = Z_SV("main"), .commit = Z_SV("HEAD"), .build = Z_BUILD_DEBUG };
+    ZapVersion v = { .branch = Z_SV("main"), .revspec = Z_SV("HEAD"), .build = Z_BUILD_DEBUG };
     ZStringBuf sb;
     z_strbuf_init(&sb);
     z_format_zap_version(v, &sb);
