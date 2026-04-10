c09412033875b890fe07bd7d591d7b117c726c7c
Maqi-x <186316794+maqi-x@users.noreply.github.com>
2026-04-10 16:38:09 +0200

    feat(zapup/app/cmd/help): add support for help <cmd>

--- src/zapup/app/cmd/help.c
+++ src/zapup/app/cmd/help.c
@@ -6,8 +6,17 @@
 #include <zapup/output.h>
 
 int zapup_exec_help(ZapupApp* app) {
-    (void) app;
     ZHelpInfo info = zapup_get_help();
+    ZCliCommand target = app->args.cmd_args.help.target;
+
+    if (target != Z_CLI_CMD_UNKNOWN) {
+        const ZHelpCommand* cmd = zapup_find_help_command(&info, target);
+        if (cmd != NULL) {
+            z_cli_print_help_cmd(&info, cmd, stdout);
+            return 0;
+        }
+    }
+
     z_cli_print_help(&info, stdout);
     return 0;
 }
