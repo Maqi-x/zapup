b4d96aa45d4c6382c7bf260d460fbb7ef494168e
Maqi-x <186316794+maqi-x@users.noreply.github.com>
2026-04-10 16:47:15 +0200

    refactor(zapup): move help logic to new function

--- src/zapup/app/cmd/help.c
+++ src/zapup/app/cmd/help.c
@@ -1,22 +1,11 @@
 #include <zapup/app/app.h>
 
-#include <zapup/cli/help/settings.h>
-#include <zapup/cli/help/print.h>
-
+#include <zapup/help.h>
 #include <zapup/output.h>
 
 int zapup_exec_help(ZapupApp* app) {
     ZHelpInfo info = zapup_get_help();
     ZCliCommand target = app->args.cmd_args.help.target;
-
-    if (target != Z_CLI_CMD_UNKNOWN) {
-        const ZHelpCommand* cmd = zapup_find_help_command(&info, target);
-        if (cmd != NULL) {
-            z_cli_print_help_cmd(&info, cmd, stdout);
-            return 0;
-        }
-    }
-
-    z_cli_print_help(&info, stdout);
+    zapup_show_help(&info, target, stdout);
     return 0;
 }
