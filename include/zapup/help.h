369b6fccac9cec9583c1a901603abc838ee19b9f
Maqi-x <186316794+maqi-x@users.noreply.github.com>
2026-04-10 16:51:53 +0200

    fix(zapup/help): move implementation from header to source file

--- include/zapup/help.h
+++ include/zapup/help.h
@@ -3,14 +3,4 @@
 #include <zapup/cli/help/print.h>
 #include <zapup/cli/args.h>
 
-void zapup_show_help(const ZHelpInfo* info, ZCliCommand target, FILE* out) {
-    if (target != Z_CLI_CMD_UNKNOWN) {
-        const ZHelpCommand* cmd = zapup_find_help_command(info, target);
-        if (cmd != NULL) {
-            z_cli_print_help_cmd(info, cmd, out);
-            return;
-        }
-    }
-
-    z_cli_print_help(info, out);
-}
+void zapup_show_help(const ZHelpInfo* info, ZCliCommand target, FILE* out);
---
+++ src/zapup/help.c
@@ -0,0 +1,13 @@
+#include <zapup/help.h>
+
+void zapup_show_help(const ZHelpInfo* info, ZCliCommand target, FILE* out) {
+    if (target != Z_CLI_CMD_UNKNOWN) {
+        const ZHelpCommand* cmd = zapup_find_help_command(info, target);
+        if (cmd != NULL) {
+            z_cli_print_help_cmd(info, cmd, out);
+            return;
+        }
+    }
+
+    z_cli_print_help(info, out);
+}
