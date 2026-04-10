ad0480e8cb7e73564583182cba684d42efe28096
Maqi-x <186316794+maqi-x@users.noreply.github.com>
2026-04-10 14:15:46 +0200

    feat(zapup/shims): add shim generation functions

---
+++ include/zapup/shims.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <util/strbuf.h>
+#include <util/pathview.h>
+
+#include <zapup/zap/toolchain.h>
+
+bool z_generate_sh_shim_for(ZPathView self, ZapToolchainElement tool, ZStringBuf* out);
+bool z_generate_batch_shim_for(ZPathView self, ZapToolchainElement elem, ZStringBuf* out);
