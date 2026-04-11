a6d10caebda102d53958ff6f10be23e5a02f2c33
Maqi-x <186316794+maqi-x@users.noreply.github.com>
2026-04-11 10:41:07 +0200

    feat(tests/cli): add argument parser tests

---
+++ tests/test-cli.c
@@ -0,0 +1,163 @@
+#include "utility.h"
+#include <unity.h>
+
+#include <zapup/cli/parse.h>
+#include <util/arr-len.h>
+
+void setUp(void) {}
+void tearDown(void) {}
+
+#define ASSERT_PARSE_OK(argc, argv, out) \
+    do { \
+        ZCliParseResult res = z_cli_parse_args(argc, (const char* const*)argv, out); \
+    if (res.code != Z_CLI_PARSE_OK) { \
+            z_parse_result_print(&res, Z_OUTPUT_ERROR); \
+        } \
+        TEST_ASSERT_EQUAL_UINT_MESSAGE(Z_CLI_PARSE_OK, res.code, "CLI parse failed"); \
+    } while (0)
+
+void test_parse_install() {
+    const char* argv[] = { "zapup", "install", "v0.1.1" };
+    ZCliArgs args;
+    ASSERT_PARSE_OK(Z_ARRAY_LEN(argv), argv, &args);
+    
+    TEST_ASSERT_EQUAL_INT(Z_CLI_CMD_INSTALL, args.cmd);
+    TEST_ASSERT_EQUAL_INT(Z_REF_REVSPEC, args.cmd_args.install.version.ref_kind);
+    TEST_ASSERT_EQUAL_SV(Z_SV("v0.1.1"), args.cmd_args.install.version.revspec);
+}
+
+void test_parse_install_flags() {
+    const char* argv[] = { "zapup", "install", "latest", "--parallel=4", "--test" };
+    ZCliArgs args;
+    ASSERT_PARSE_OK(Z_ARRAY_LEN(argv), argv, &args);
+    
+    TEST_ASSERT_EQUAL_INT(Z_CLI_CMD_INSTALL, args.cmd);
+    TEST_ASSERT_EQUAL_INT(Z_REF_LATEST, args.cmd_args.install.version.ref_kind);
+    TEST_ASSERT_TRUE(args.cmd_args.install.build.parallel);
+    TEST_ASSERT_EQUAL_INT(4, args.cmd_args.install.build.max_jobs);
+    TEST_ASSERT_TRUE(args.cmd_args.install.test);
+}
+
+void test_parse_uninstall() {
+    const char* argv[] = { "zapup", "uninstall", "v0.1.1" };
+    ZCliArgs args;
+    ASSERT_PARSE_OK(Z_ARRAY_LEN(argv), argv, &args);
+    
+    TEST_ASSERT_EQUAL_INT(Z_CLI_CMD_UNINSTALL, args.cmd);
+    TEST_ASSERT_EQUAL_INT(Z_REF_REVSPEC, args.cmd_args.uninstall.version.ref_kind);
+    TEST_ASSERT_EQUAL_SV(Z_SV("v0.1.1"), args.cmd_args.uninstall.version.revspec);
+}
+
+void test_parse_switch() {
+    const char* argv[] = { "zapup", "switch", "v0.1.1" };
+    ZCliArgs args;
+    ASSERT_PARSE_OK(Z_ARRAY_LEN(argv), argv, &args);
+    
+    TEST_ASSERT_EQUAL_INT(Z_CLI_CMD_SWITCH, args.cmd);
+    TEST_ASSERT_EQUAL_INT(Z_REF_REVSPEC, args.cmd_args.switch_.version.ref_kind);
+    TEST_ASSERT_EQUAL_SV(Z_SV("v0.1.1"), args.cmd_args.switch_.version.revspec);
+}
+
+void test_parse_reshim() {
+    {
+        const char* argv[] = { "zapup", "reshim" };
+        ZCliArgs args;
+        ASSERT_PARSE_OK(Z_ARRAY_LEN(argv), argv, &args);
+        TEST_ASSERT_EQUAL_INT(Z_CLI_CMD_RESHIM, args.cmd);
+        TEST_ASSERT_EQUAL_INT(Z_TOOLCHAIN_ELEMENT_UNKNOWN, args.cmd_args.reshim.tool);
+    }
+    {
+        const char* argv[] = { "zapup", "reshim", "zapc" };
+        ZCliArgs args;
+        ASSERT_PARSE_OK(Z_ARRAY_LEN(argv), argv, &args);
+        TEST_ASSERT_EQUAL_INT(Z_CLI_CMD_RESHIM, args.cmd);
+        TEST_ASSERT_EQUAL_INT(Z_TOOLCHAIN_ELEMENT_ZAPC, args.cmd_args.reshim.tool);
+    }
+}
+
+void test_parse_which() {
+    {
+        const char* argv[] = { "zapup", "which", "zapc" };
+        ZCliArgs args;
+        ASSERT_PARSE_OK(Z_ARRAY_LEN(argv), argv, &args);
+        TEST_ASSERT_EQUAL_INT(Z_CLI_CMD_WHICH, args.cmd);
+        TEST_ASSERT_TRUE(z_zap_ver_is_null(args.cmd_args.which.version));
+        TEST_ASSERT_EQUAL_INT(Z_TOOLCHAIN_ELEMENT_ZAPC, args.cmd_args.which.tool);
+    }
+    {
+        const char* argv[] = { "zapup", "which", "v0.1.1", "zap-lsp" };
+        ZCliArgs args;
+        ASSERT_PARSE_OK(Z_ARRAY_LEN(argv), argv, &args);
+        TEST_ASSERT_EQUAL_INT(Z_CLI_CMD_WHICH, args.cmd);
+        TEST_ASSERT_EQUAL_INT(Z_REF_REVSPEC, args.cmd_args.which.version.ref_kind);
+        TEST_ASSERT_EQUAL_SV(Z_SV("v0.1.1"), args.cmd_args.which.version.revspec);
+        TEST_ASSERT_EQUAL_INT(Z_TOOLCHAIN_ELEMENT_ZAP_LSP, args.cmd_args.which.tool);
+    }
+}
+
+void test_parse_list() {
+    const char* argv[] = { "zapup", "list" };
+    ZCliArgs args;
+    ASSERT_PARSE_OK(Z_ARRAY_LEN(argv), argv, &args);
+    TEST_ASSERT_EQUAL_INT(Z_CLI_CMD_LIST, args.cmd);
+}
+
+void test_parse_show() {
+    const char* argv[] = { "zapup", "show" };
+    ZCliArgs args;
+    ASSERT_PARSE_OK(Z_ARRAY_LEN(argv), argv, &args);
+    TEST_ASSERT_EQUAL_INT(Z_CLI_CMD_SHOW, args.cmd);
+}
+
+void test_parse_help() {
+    {
+        const char* argv[] = { "zapup", "help" };
+        ZCliArgs args;
+        ASSERT_PARSE_OK(Z_ARRAY_LEN(argv), argv, &args);
+        TEST_ASSERT_EQUAL_INT(Z_CLI_CMD_HELP, args.cmd);
+        TEST_ASSERT_EQUAL_INT(Z_CLI_CMD_UNKNOWN, args.cmd_args.help.target);
+    }
+    {
+        const char* argv[] = { "zapup", "help", "install" };
+        ZCliArgs args;
+        ASSERT_PARSE_OK(Z_ARRAY_LEN(argv), argv, &args);
+        TEST_ASSERT_EQUAL_INT(Z_CLI_CMD_HELP, args.cmd);
+        TEST_ASSERT_EQUAL_INT(Z_CLI_CMD_INSTALL, args.cmd_args.help.target);
+    }
+}
+
+void test_parse_sync() {
+    {
+        const char* argv[] = { "zapup", "sync" };
+        ZCliArgs args;
+        ASSERT_PARSE_OK(Z_ARRAY_LEN(argv), argv, &args);
+        TEST_ASSERT_EQUAL_INT(Z_CLI_CMD_SYNC, args.cmd);
+        TEST_ASSERT_TRUE(z_zap_ver_is_null(args.cmd_args.sync.version));
+    }
+}
+
+void test_parse_test() {
+    {
+        const char* argv[] = { "zapup", "test" };
+        ZCliArgs args;
+        ASSERT_PARSE_OK(Z_ARRAY_LEN(argv), argv, &args);
+        TEST_ASSERT_EQUAL_INT(Z_CLI_CMD_TEST, args.cmd);
+        TEST_ASSERT_TRUE(z_zap_ver_is_null(args.cmd_args.test.version));
+    }
+}
+
+int main(void) {
+    UNITY_BEGIN();
+    RUN_TEST(test_parse_install);
+    RUN_TEST(test_parse_install_flags);
+    RUN_TEST(test_parse_uninstall);
+    RUN_TEST(test_parse_switch);
+    RUN_TEST(test_parse_reshim);
+    RUN_TEST(test_parse_which);
+    RUN_TEST(test_parse_list);
+    RUN_TEST(test_parse_show);
+    RUN_TEST(test_parse_help);
+    RUN_TEST(test_parse_sync);
+    RUN_TEST(test_parse_test);
+    return UNITY_END();
+}
