8b6c5969d6add4b43a761a073d10cc848d474a82
Maqi-x <186316794+maqi-x@users.noreply.github.com>
2026-04-12 19:21:34 +0200

    feat(zapup/app): support for local project config (.zapup.json)

--- src/zapup/app/app.c
+++ src/zapup/app/app.c
@@ -11,6 +11,7 @@
 #include <zapup/output.h>
 #include <zapup/help.h>
 
+#include <util/fs.h>
 #include <git2.h>
 
 void zapup_init(ZapupApp* app) {
@@ -21,7 +22,22 @@ void zapup_init(ZapupApp* app) {
     z_version_index_load(&app->index, z_pathbuf_as_view(&app->paths.indexfile));
     z_lockfile_init(&app->indexlock);
     z_config_init(&app->cfg);
-    z_config_load(&app->cfg, z_pathbuf_as_view(&app->paths.cfgfile));
+}
+
+int zapup_load_config(ZapupApp* app) {
+    if (!z_config_load(&app->cfg, z_pathbuf_as_view(&app->paths.cfgfile))) {
+        return 1;
+    }
+
+    if (!app->args.global_args.ignore_local && z_file_exists(Z_PV("./.zapup.json"))) {
+        if (!z_config_merge_load(&app->cfg, Z_PV("./.zapup.json"))) {
+            return 1;
+        }
+    }
+
+    z_show_debug("CC:  " Z_SV_FMT, Z_SV_FARG(app->cfg.build.cc));
+    z_show_debug("CXX: " Z_SV_FMT, Z_SV_FARG(app->cfg.build.cxx));
+    return 0;
 }
 
 int zapup_run(ZapupApp* app, int argc, const char* const* argv) {
@@ -31,6 +47,11 @@ int zapup_run(ZapupApp* app, int argc, const char* const* argv) {
         ZHelpInfo info = zapup_get_help();
         zapup_show_help(&info, app->args.cmd, stderr);
         return 1;
+    }
+
+    int result = zapup_load_config(app);
+    if (result != 0) {
+        return result;
     }
 
     switch (app->args.cmd) {
