fa9c82b8c9f1f083d1651df69a594111ecb20be1
Maqi-x <186316794+maqi-x@users.noreply.github.com>
2026-04-28 15:55:49 +0200

    feat(zapup/zap/lsp): add nvim lsp mode definition and associated methods

--- include/zapup/zap/lsp.h
+++ include/zapup/zap/lsp.h
@@ -8,6 +8,12 @@ typedef enum ZapLspTarget {
     Z_LSP_VSC     = 1 << 1,
 } ZapLspTarget;
 
+typedef enum ZapNvimLspMode {
+    Z_NVIM_LSP_INIT_LUA,
+    Z_NVIM_LSP_LAZY,
+    Z_NVIM_LSP_UNKNOWN,
+} ZapNvimLspMode;
+
 static inline ZStringView z_format_zap_lsp_target(ZapLspTarget target) {
     switch (target) {
     case Z_LSP_NVIM:
@@ -26,5 +32,26 @@ static inline ZapLspTarget z_parse_zap_lsp_target(ZStringView str) {
         return Z_LSP_VSC;
     } else{
         return Z_LSP_UNKNOWN;
+    }
+}
+
+static inline ZStringView z_format_nvim_lsp_mode(ZapNvimLspMode mode) {
+    switch (mode) {
+    case Z_NVIM_LSP_INIT_LUA:
+        return Z_SV("init.lua");
+    case Z_NVIM_LSP_LAZY:
+        return Z_SV("lazy");
+    case Z_NVIM_LSP_UNKNOWN:
+        return Z_SV_NULL;
+    }
+}
+
+static inline ZapNvimLspMode z_parse_nvim_lsp_mode(ZStringView str) {
+    if (z_sv_eql(str, Z_SV("init.lua")) || z_sv_eql(str, Z_SV("init"))) {
+        return Z_NVIM_LSP_INIT_LUA;
+    } else if (z_sv_eql(str, Z_SV("lazy")) || z_sv_eql(str, Z_SV("lazy.nvim"))) {
+        return Z_NVIM_LSP_LAZY;
+    } else{
+        return Z_NVIM_LSP_UNKNOWN;
     }
 }
